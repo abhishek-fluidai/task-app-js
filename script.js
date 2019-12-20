186af0f80208b55542e5937c1b7758f4f4bfff49
Mabhishek7081 <mabhishek7081@gmail.com>
2019-12-20 18:43:29 +0530

    vvv

--- script.js
+++ script.js
@@ -1,43 +1,123 @@
   const inp = document.getElementById('input');
   const btn = document.getElementById('addBtn');
+  let taskList =  document.getElementById('todo-list')
+  let taskArrey =[];
+  let completedTasks = [];
   let ids = 0;
     function addTask() {
         let val = inp.value;
+        
         val== "" ||val == null ? alert('Please enter a Task!') : 
-        document.getElementById('todo-list').innerHTML += `
-        <li class="todo-item " id="${ids+1}" >
-        <input type="checkbox" class="${ids+1}"  onclick="completeTask(${ids+1})"/> <span class="itemi">${val} </span>
+        taskList.innerHTML += `
+        <li class="todo-item false" id="${ids+1}" >
+        <input type="checkbox" class="${ids+1}"   onclick="completeTask(${ids+1})"/> <span class="itemi">${val} </span>
         <span class="close-icon" onclick="removeTask(${ids+1})">&#10006;</span>
     </li>`;
+      // Storing Dat In Localstorage
+     taskArrey.push({"id": `${ids+1}`, "contant": `${val}`, "completed": "false"});  
+     localStorage.setItem('taskArrey', JSON.stringify(taskArrey));
         inp.value = "";
         ids += 1;
       
     }
     const completeTask = (e) => {
-        
         let task = document.getElementById(e);
         let check = document.getElementsByClassName(e)[0];
-        // if (check.checked== true) {
-        //   task.classList.add('checked')
-        // }
-        // else if (check.checked == false) {
-        //   task.classList.remove('checked')
-        // }
-       check.checked ? task.classList.add('checked') : task.classList.remove('checked')
+        if (check.checked== true ) {
+         task.classList.add('checked');
+         task.classList.remove('false');
+        //  check.checked = "checked"
+         
+          for (var i =0; i < taskArrey.length; i++)
+          if (taskArrey[i].id == e) {
+              taskArrey[i].completed = "checked"
+              // taskArrey[i].checked = "checked"
+                 completedTasks.push(i)
+              
+              break;
+         }
+        //  console.log(taskArrey)
+        localStorage.removeItem('taskArrey');
+        localStorage.setItem('taskArrey', JSON.stringify(taskArrey));
+        }
+        else if (check.checked == false ) {
+          task.classList.remove('checked');
+          task.classList.add('false');
+          // check.checked = ""
+          for (var i =0; i < taskArrey.length; i++)
+          if (taskArrey[i].id == e) {
+              taskArrey[i].completed = "false"
+              var index = completedTasks.indexOf(i);
+            if (index !== -1) completedTasks.splice(index, 1);
+            
+              break;
+         }
+         localStorage.removeItem('taskArrey');
+        localStorage.setItem('taskArrey', JSON.stringify(taskArrey));
+
+         
+        }
+      //  check.checked ? task.classList.add('checked') : task.classList.remove('checked')
     }
 
     function removeTask(e) {
       var a = document.getElementById(e);
-        // a.style.display = "none"
-        a.remove()
-    }
-    const clearAlltasks = () => {
-      let a = document.querySelectorAll('.checked');
-      for(let i=0; i< a.length; i++){
-        a[i].remove();
-        console.log(i)
-    }
+              // a.style.display = "none"
+        a.remove();
+        for (var i =0; i < taskArrey.length; i++)
+        if (taskArrey[i].id == e) {
+            taskArrey.splice(i,1);
+            break;
+        }
+        localStorage.removeItem('taskArrey');
+        localStorage.setItem('taskArrey', JSON.stringify(taskArrey));
+
       }
+
+     const loadTasks = () => {
+      let addedTasks = JSON.parse(localStorage.getItem('taskArrey'));
+       for (let i =0; i < addedTasks.length; i++) {
+          console.log(addedTasks[i])
+          taskArrey.push({"id": `${addedTasks[i].id}`, "contant": `${addedTasks[i].contant}`, "completed":`${addedTasks[i].completed}`}); 
+        taskList.innerHTML += `
+        <li class="todo-item ${addedTasks[i].completed}" id="${addedTasks[i].id}" >
+        <input type="checkbox" class="${addedTasks[i].id}"  onclick="completeTask(${addedTasks[i].id})"/> <span class="itemi"> ${addedTasks[i].contant}</span>
+        <span class="close-icon" onclick="removeTask(${addedTasks[i].id})">&#10006;</span>
+    </li>`;
+ 
+    if (addedTasks[i].completed == "checked" ) {
+    document.getElementsByClassName(addedTasks[i].id)[0].setAttribute('checked', 'true')
+         }
+       }
+     }
+
+   
+  //   const clearAlltasks = () => {
+   
+  //     let a = document.querySelectorAll('.todo-item.checked');
+  //       for (var v =0; v < completedTasks.length; v++) {
+  //         a[v].remove();
+                   
+  //         for (var i =0; i < taskArrey.length; i++) {
+  //              if (taskArrey[i].id == v) {
+  //                   taskArrey.splice(i,1);
+  //                   console.log(taskArrey[i])
+  //                   localStorage.removeItem('taskArrey');
+  //                   localStorage.setItem('taskArrey', JSON.stringify(taskArrey));
+                    
+  //               }
+                
+  //             }
+                         
+  //             }
+  //             // localStorage.removeItem('taskArrey');
+  //             // localStorage.setItem('taskArrey', JSON.stringify(taskArrey));
+       
+          
+        
+  // }
+  
+
 
       document.addEventListener("keypress", (e) => {
         if (e.keyCode == 13 && inp.value !== "") {
@@ -47,9 +127,12 @@
           alert('Please enter a Task!')
         }
       });
-
+      window.addEventListener('load', () => {
+        console.log('page is fully loaded');
+        loadTasks();
+      });
  
-//PWA Settings 
+// PWA Settings 
 window.onload = () => {
   'use strict';
 
